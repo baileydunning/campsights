9264d6669827aedc9d3bff325855156f3e0fd844
Bailey Dunning <bwdunning@gmail.com>
2025-06-14 13:52:24 -0600

    add campsiteform test

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    globals: true,
+    setupFiles: './utils/setupTests.ts',
+    globals: true
   },
 });
