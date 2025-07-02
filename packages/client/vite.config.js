aa733c2c96b716750c3bf9716dbcfa9d42b6c6bc
Bailey Dunning <bwdunning@gmail.com>
2025-07-02 16:56:44 -0600

    optimize vite build

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   server: {
     port: 5173,
     proxy: {
       "/api": "http://localhost:3000",
+    },
+  },
+  build: {
+    target: "esnext",
+    sourcemap: false,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          leaflet: ["leaflet", "react-leaflet"],
+        },
+      },
     },
   },
 });
