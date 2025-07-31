88587ac64a552851f37a9ad8363153a84468c51e
Bailey Dunning <bwdunning@gmail.com>
2025-07-31 11:18:32 -0600

    rm cluster stuff

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,28 +1,10 @@
-import cluster from 'cluster';
-import os from 'os';
 import { server } from './server';
 
-const numCPUs = os.cpus().length;
-const totalMem = os.totalmem(); // in bytes
-const perWorkerMem = 256 * 1024 * 1024; 
-const maxWorkersByMem = Math.floor(totalMem / perWorkerMem);
-const numWorkers = Math.max(1, Math.min(numCPUs, maxWorkersByMem));
 const PORT = process.env.PORT || 3000;
+const app = server();
 
-if (cluster.isPrimary) {
-  console.log(`Master ${process.pid} is running`);
-  for (let i = 0; i < numWorkers; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', (worker) => {
-    console.warn(`Worker ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  const app = server();
-  app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export { server };
